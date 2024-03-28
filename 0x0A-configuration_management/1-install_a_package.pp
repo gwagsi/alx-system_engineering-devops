5643378830880124f3c4d50773d02dc764312dd6
gwagsi <gwagsiglenn@gmail.com>
2024-03-27 19:56:38 -0700

    sam

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
-# Installs puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => gem,
+# Define a resource class for managing pip packages
+class { 'pip3::package': }
+
+# Install flask package with specific version
+pip3::package { 'flask':
+  ensure    => '2.1.0',
+  provider  => 'pip',
 }
+
